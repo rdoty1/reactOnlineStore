96b25622074573a7bf19a60539da63cb61b34ab9
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-05 21:54:53 -0400

    updates to initial files, as well as getting bootstrap and font awesome set up

---
+++ juul/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+   <div className="container">
+     <div className="row">
+       <div className="col-6">column one </div>
+       <div className="col-6">
+         <span>
+            <i className="fas fa-home" />
+         </span>
+
+        </div>
+     </div>
+
+
+   </div>
+  );
+}
+
+export default App;
