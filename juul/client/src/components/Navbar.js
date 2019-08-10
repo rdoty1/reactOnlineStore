8612505df9e1e5eb8768256413fb7fe046c3a40b
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-09 22:31:10 -0400

    moreup

--- juul/client/src/components/Navbar.js
+++ juul/client/src/components/Navbar.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg'
 import styled from 'styled-components'
+import {ButtonContainer} from  './Button'
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-small bg-primary navvbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-small navvbar-dark px-sm-5">
                 {/* 
                 https://www.iconfinder.com/icons/1243689/call_phone_icon
                 Creative Commons (Attribution 3.0 Unported);
@@ -37,33 +38,21 @@ class Navbar extends Component {
 
                 </Link>
                
-
-
-
-            </nav>
+            </NavWrapper>
         );
     }
 }
 
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size:1.4rem;
-background: transparent;
-border:0.05rem solid var(--lightBlue);
-color: var(--lightBlue);
-border-radius:0.5rem;
-padding: 0.2rem 0.5rem;
-cursor:pointer;
-margin:0.2rem 0.5rem 0.2rem  0;
-transition:all 0.5s ease-in-out;
-&:hover {
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-
-}
-&:focus {
-    outline:none;
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link {
+    color: var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform: capitalize;
+    
 }
 `
+
+
 
 export default Navbar;
