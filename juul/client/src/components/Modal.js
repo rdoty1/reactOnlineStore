17982013aa2d03310c04c57a2d889d5499e3102f
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-11 11:05:41 -0400

    starting to set up the cart

--- juul/client/src/components/Modal.js
+++ juul/client/src/components/Modal.js
@@ -20,9 +20,23 @@ class Modal extends Component {
                         <ModalContainer>
                             <div className="container">
                                 <div className="row">
-                                    <div className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
+                                    <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
                                         <h5> item added to the cart</h5>
+                                        <img src={img} className="img-fluid" alt="product" />
+                                        <h5>{title}</h5>
+                                        <h5 className="text-muted"> price : $ {price} </h5>
+                                        <Link to='/'>
+                                            <ButtonContainer onClick={() => closeModal()}>
+                                                Store
+                                            </ButtonContainer>
+                                        </Link>
+                                        <Link to='/cart'>
+                                            <ButtonContainer cart onClick={() => closeModal()}>
+                                                go to cart
+                                            </ButtonContainer>
+                                        </Link>
                                     </div>
+
                                 </div>
                             </div>
 
@@ -41,14 +55,23 @@ class Modal extends Component {
 
 const ModalContainer = styled.div`
 position:fixed;
-top:0;
-left:0;
-right:0;
-bottom:0;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
 background: rgba(0,0,0,0.3);
 display:flex;
 aligh-items:center;
 justify-content: center;
+
+#modal {
+    background: var(--mainWhite);
+
+
+}
+
+
+
 
 `;
 
