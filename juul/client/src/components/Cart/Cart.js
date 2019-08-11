f3fd28a994f58167d76eea4491ea6605ae354b45
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-11 13:58:17 -0400

    new updates to the cart in regards to subtotal

--- juul/client/src/components/Cart/Cart.js
+++ juul/client/src/components/Cart/Cart.js
@@ -4,7 +4,7 @@ import CartColumns from './CartColumns';
 import EmptyCart from "./EmptyCart";
 import {ProductConsumer} from '../../context';
 import CartList from './CartList'
-
+import CartTotals from './CartTotals'
 
 class Cart extends Component {
     render() {
@@ -20,6 +20,7 @@ class Cart extends Component {
                                     <CartColumns />
 
                                     <CartList value={value}/>
+                                    <CartTotals value={value} /> 
                                 </React.Fragment>
                                 
 
--- juul/client/src/components/Cart/CartItem.js
+++ juul/client/src/components/Cart/CartItem.js
@@ -5,7 +5,7 @@ function CartItem({item,value}) {
     const {id,title,img,price,total,count} = item;
     const {increment, decrement, removeItem} = value;
     return (
-        <div className="row my-1 text-capitalize text-center">
+        <div className="row my-2 text-capitalize text-center">
 
             <div className="clo-10 mx-auto col-lg-2">
                 <img src={img} style={{width: '5rem', height:'5rems'}} className="img-fluid" alt="product" />
@@ -17,9 +17,9 @@ function CartItem({item,value}) {
                 {title}
             </div>
 
-            <div className="clo-10 mx-auto col-lg-2">
+            <div className="col-10 mx-auto col-lg-2">
             <span className="d-lg-none"> price : </span> 
-                {price}
+                ${price}
                 
             </div>
 
@@ -27,6 +27,8 @@ function CartItem({item,value}) {
                 <div className="d-flex justify-content-center">
                     <div>
                         <span className="btn btn-black mx-1" onClick={() => decrement(id)}> - </span>
+                        <span className="btn btn-black mx-1"> {count}</span>
+                        <span className="btn btn-black mx-1" onClick={() => increment(id)}> + </span>
 
                     </div>
 
@@ -34,6 +36,19 @@ function CartItem({item,value}) {
             </div>
 
             {/* end of col */}
+            <div className="col-10 mx-auto col-lg-2">
+                <div className="cart-icon" onClick={() => removeItem(id)}>
+                    <i className="fas fa-trash"></i>
+                </div>
+                
+            </div>
+
+            <div className="col-10 mx-auto col-lg-2">
+                <strong> item total: ${total} </strong>
+                
+            </div>
+
+
           
         </div>
     )
