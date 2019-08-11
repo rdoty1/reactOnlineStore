7895264982ae3f72d20bbd57ab2dbd640ccbd5ec
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-11 09:53:02 -0400

    updates to modal component, close to fully implimenting, successfully displaying on the page

--- juul/client/src/components/Product.js
+++ juul/client/src/components/Product.js
@@ -23,7 +23,9 @@ class Product extends Component {
                                     <img src={img} alt="product" className="card-img-top"/>
                                 </Link>
 
-                                <button className="cart-btn" disabled={inCart? true : false } onClick={() =>{value.addToCart(id);}}>
+                                <button className="cart-btn" disabled={inCart? true : false } onClick={() =>
+                                    {value.addToCart(id);
+                                     value.openModal(id); }}>
                                     {inCart?(
                                         <p className="text-capitalize mb-0" disabled>
                                             {" "}  
